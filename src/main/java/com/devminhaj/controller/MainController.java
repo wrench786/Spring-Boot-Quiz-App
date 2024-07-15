a42ef0a8753c003f037e7c7209bcb43237258e98
Md. Minhajul Islam <wrench786@gmail.com>
2024-07-15 20:37:25 +0600

    Version 1.2

--- src/main/java/com/devminhaj/controller/MainController.java
+++ src/main/java/com/devminhaj/controller/MainController.java
@@ -1,11 +1,10 @@
 package com.devminhaj.controller;
 
+import com.devminhaj.model.QuestionForm;
 import com.devminhaj.service.QuizService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -21,12 +20,21 @@ public class MainController {
     }
 
     @PostMapping("/quiz")
-    public String quiz(@RequestParam String name, RedirectAttributes redirectAttributes){
+    public String quiz(@RequestParam String name, RedirectAttributes redirectAttributes, Model model){
         if(name.isBlank()){
             redirectAttributes.addFlashAttribute("message", "Name cannot be blank");
             return "redirect:/home";
         }
+        QuestionForm questionForm = new QuestionForm();
+        questionForm.setQuestions(quizService.getQuestion());
+        model.addAttribute("questionForm", questionForm);
         return "quiz";
     }
 
+    @PostMapping("/submit")
+    public String submit(@ModelAttribute QuestionForm questionForm){
+
+        System.out.println();
+        return "resultPage";
+    }
 }
---
+++ src/main/java/com/devminhaj/model/QuestionForm.java
@@ -0,0 +1,12 @@
+package com.devminhaj.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class QuestionForm {
+    private List<Question> questions;
+}
